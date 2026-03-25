d6c7b5dfcf6b5bba7097f3f528c6c41343992502
Aadarsh <aadarshthakur900@gmail.com>
2026-03-25 13:34:27 +0530

    added UC5 feature

---
+++ UC5/helloAppUC5.java
@@ -0,0 +1,16 @@
+package HelloApp.UC5;
+
+public class helloAppUC5 {
+    public static void main(String[] args) {
+
+        // If no arguments, greet "World"
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Enhanced for loop to process multiple names
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+    }
+}
